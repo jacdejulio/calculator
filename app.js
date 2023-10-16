698c86511c18c8c68154457c831a3d6a32e9700d
jacdejulio <76726688+jacdejulio@users.noreply.github.com>
2023-10-16 14:38:51 +0800

    Added Clear and All Clear functionalities

--- app.js
+++ app.js
@@ -1,20 +1,56 @@
 const keys = document.querySelector(".calc_keys");
 const digits = document.querySelectorAll(".digit");
 const displayB = document.querySelector(".calc_display_b");
-let displayBVal = displayB.innerText;
+let displayBVal = displayB.textContent;
+const displayA = document.querySelector(".calc_display_a");
+let displayAVal = displayA.textContent;
 
 
 keys.addEventListener('click', e => {
-    if (e.target.classList.contains('digit')) {
+    let currDisplay = '';
+    if (e.target.classList.contains('digit') || e.target.className === "decimal") {
         if (displayBVal === '0') {
             displayBVal = e.target.textContent;
         } else {
-            // Append the clicked digit to the existing content
             displayBVal += e.target.textContent;
         }
-        // Update the display with the new value
-        displayB.innerText = displayBVal;
+
+    }
+    if (e.target.className === "all-clear") {
+        displayBVal = '0';
+        displayAVal = '';
+    }
+
+    if (e.target.className === "clear") {
+        if (displayBVal.length > 1) {
+            displayBVal = displayBVal.slice(0, displayBVal.length - 1);
+
+        } else {
+            displayBVal = '0';
+        }
+
+    }
+
+    if (e.target.className === "add") {
+        displayAVal = displayBVal + e.target.textContent;
+        displayBVal = '0'
+    }
+
+    if (e.target.className === "equal") {
+        displayAVal = parseFloat(displayAVal.slice(0, displayAVal.length - 1));
+        displayBVal = parseFloat(displayBVal);
+        result = add(displayAVal, displayBVal);
+        displayBVal = result;
     }
 
 
+    displayB.textContent = displayBVal;
+    displayA.textContent = displayAVal;
+
+
 });
+
+
+function add(a, b) {
+    return a + b;
+}
