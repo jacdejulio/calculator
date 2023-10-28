4ca512929f73765b2dd310a3db3987c057b6277e
jacdejulio <76726688+jacdejulio@users.noreply.github.com>
2023-10-28 19:29:43 +0800

    Keyboard Support for happy path

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-
-// KB support
-
-
 const displayNum1 = document.querySelector('.display-num1')
 const displayNum2 = document.querySelector('.display-num2')
 const displayOperator = document.querySelector('.display-operator')
@@ -11,6 +7,7 @@ const keys = document.querySelector('.keys');
 const equalBtn = document.querySelector('.equal');
 const equalBtnDefBg = '#6c757d';
 const equalBtnClkBg = '#f6b327';
+
 
 keys.addEventListener('click', e => {
     const eTarget = e.target;
@@ -22,12 +19,14 @@ keys.addEventListener('click', e => {
     let equalContent = displayEqual.textContent;
 
     if (eTarget.classList.contains('digit')) {
+        //Add btn textcontent to num1
         if (num1Content === '0' &&
             operatorContent === '') {
             displayNum1.textContent = keyText;
         } else if (num1Content !== '0' &&
             operatorContent === '') {
             displayNum1.textContent += keyText;
+            //Add btn textcontent to num2
         } else if (num2Content === '0' &&
             num2Content.length === 1 &&
             equalContent === '') {
@@ -36,6 +35,7 @@ keys.addEventListener('click', e => {
             operatorContent !== '' &&
             equalContent === '') {
             displayNum2.textContent += keyText;
+            //Add btn textcontent to num1 after pressing equal sign
         } else if (numResult !== '' && num2Content !== '') {
             resetValues();
             displayNum1.textContent = ''
@@ -124,7 +124,6 @@ keys.addEventListener('click', e => {
             displayNum1.textContent = '0';
         } else if (equalContent !== '') {
             resetValues();
-            displayNum1.textContent = '0';
         } else if (num2Content.length > 1 &&
             operatorContent !== '') {
             displayNum2.textContent =
@@ -135,6 +134,72 @@ keys.addEventListener('click', e => {
         }
     }
 })
+
+document.addEventListener('keydown', e => {
+    // KB support
+    // Happy path = num1 + operator + num2 + equal sign
+    let operateResult = '';
+    let eventKey = e.key;
+    const isNumber = isFinite(eventKey);
+    let num1Content = displayNum1.textContent;
+    let num2Content = displayNum2.textContent;
+    let numResult = displayResult.textContent;
+    let operatorContent = displayOperator.textContent;
+    let equalContent = displayEqual.textContent;
+
+
+    if (operatorContent !== '' && equalContent === '' &&
+        isNumber) {
+        displayNum2.textContent += eventKey;
+    } else if (num1Content === '0' &&
+        isNumber) {
+        displayNum1.textContent = eventKey;
+    } else if (num1Content !== '0' && equalContent === '' &&
+        isNumber) {
+        displayNum1.textContent += eventKey;
+    }
+
+    if (eventKey === '+' || eventKey === '-' &&
+        equalContent === '') {
+        displayOperator.textContent = eventKey;
+    } else if (equalContent === '' &&
+        eventKey === '/') {
+        displayOperator.textContent = '÷';
+    } else if (equalContent === '' &&
+        eventKey === '*') {
+        displayOperator.textContent = '×';
+    }
+
+    if (eventKey === '=' || eventKey === 'Enter'
+        && operatorContent !== ''
+        && num2Content !== '') {
+        displayEqual.textContent = '=';
+        equalBtn.style.background = equalBtnClkBg;
+        operateResult = operate(num1Content, num2Content, operatorContent);
+        displayResult.textContent = operateResult;
+    }
+
+
+    if (eventKey === '.' && operatorContent === '' &&
+        !num1Content.includes('.') && num2Content === '') {
+        displayNum1.textContent += eventKey;
+    } else if (eventKey === '.' && num2Content.length >= 1 &&
+        !num2Content.includes('.') && operatorContent !== '' &&
+        equalContent === '') {
+        displayNum2.textContent += eventKey;
+    } else if (eventKey === '.' && num2Content === '' &&
+        !num2Content.includes('.') && operatorContent !== '') {
+        displayNum2.textContent += `0${eventKey}`;
+    }
+
+    if (eventKey === 'Escape') {
+        resetValues();
+    }
+})
+
+
+
+
 
 const resetValues = () => {
     displayNum1.textContent = '0';
