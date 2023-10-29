76960571b3a2cc02ebf5fb49deb2734d142e41a5
jacdejulio <76726688+jacdejulio@users.noreply.github.com>
2023-10-29 14:07:10 +0800

    Modified kb event to work as same as mouse event

--- app.js
+++ app.js
@@ -69,9 +69,6 @@ keys.addEventListener('click', e => {
                 displayNum1.textContent = operateResult;
                 displayNum2.textContent = '';
             }
-
-
-
             //Press operator after num1
         } else if (num1Content !== '' && num2Content === '') {
             displayOperator.textContent = keyText;
@@ -103,7 +100,6 @@ keys.addEventListener('click', e => {
         } //Press operator after operate()
         else if (numResult !== '' &&
             equalContent !== '') {
-            // displayResult.textContent = '';
             operateResult = operate(num1Content, num2Content, operatorContent);
             displayNum1.textContent = operateResult;
             displayResult.textContent = operate(operateResult, num2Content, operatorContent);
@@ -135,69 +131,142 @@ keys.addEventListener('click', e => {
     }
 })
 
+// Keyboard support
 document.addEventListener('keydown', e => {
-    // KB support
-    // Happy path = num1 + operator + num2 + equal sign
-    let operateResult = '';
-    let eventKey = e.key;
+    const eventKey = e.key;
     const isNumber = isFinite(eventKey);
     let num1Content = displayNum1.textContent;
     let num2Content = displayNum2.textContent;
     let numResult = displayResult.textContent;
     let operatorContent = displayOperator.textContent;
     let equalContent = displayEqual.textContent;
+    let operateResult = '';
 
 
-    if (operatorContent !== '' && equalContent === '' &&
-        isNumber) {
-        displayNum2.textContent += eventKey;
-    } else if (num1Content === '0' &&
-        isNumber) {
-        displayNum1.textContent = eventKey;
-    } else if (num1Content !== '0' && equalContent === '' &&
-        isNumber) {
-        displayNum1.textContent += eventKey;
-    }
+    if (isNumber) {
+        //Add btn textcontent to num1
+        if (num1Content === '0' &&
+            operatorContent === '') {
+            displayNum1.textContent = eventKey;
+        } else if (num1Content !== '0' &&
+            operatorContent === '') {
+            displayNum1.textContent += eventKey;
+            //Add btn textcontent to num2
+        } else if (num2Content === '0' &&
+            num2Content.length === 1 &&
+            equalContent === '') {
+            displayNum2.textContent = eventKey;
+        } else if (num1Content.length >= 1 &&
+            operatorContent !== '' &&
+            equalContent === '') {
+            displayNum2.textContent += eventKey;
+            //Add btn textcontent to num1 after pressing equal sign
+        } else if (numResult !== '' && num2Content !== '') {
+            resetValues();
+            displayNum1.textContent = ''
+            displayNum1.textContent += eventKey;
+        }
 
-    if (eventKey === '+' || eventKey === '-' &&
-        equalContent === '') {
-        displayOperator.textContent = eventKey;
-    } else if (equalContent === '' &&
-        eventKey === '/') {
-        displayOperator.textContent = '÷';
-    } else if (equalContent === '' &&
-        eventKey === '*') {
-        displayOperator.textContent = '×';
-    }
+    } else if (eventKey === '.') {
+        if (operatorContent === '' && !num1Content.includes('.') &&
+            num2Content === '') {
+            displayNum1.textContent += eventKey;
 
-    if (eventKey === '=' || eventKey === 'Enter'
-        && operatorContent !== ''
-        && num2Content !== '') {
-        displayEqual.textContent = '=';
-        equalBtn.style.background = equalBtnClkBg;
-        operateResult = operate(num1Content, num2Content, operatorContent);
-        displayResult.textContent = operateResult;
-    }
-
-
-    if (eventKey === '.' && operatorContent === '' &&
-        !num1Content.includes('.') && num2Content === '') {
-        displayNum1.textContent += eventKey;
-    } else if (eventKey === '.' && num2Content.length >= 1 &&
-        !num2Content.includes('.') && operatorContent !== '' &&
-        equalContent === '') {
-        displayNum2.textContent += eventKey;
-    } else if (eventKey === '.' && num2Content === '' &&
-        !num2Content.includes('.') && operatorContent !== '') {
-        displayNum2.textContent += `0${eventKey}`;
-    }
-
-    if (eventKey === 'Escape') {
+        } else if (num2Content.length >= 1 &&
+            !num2Content.includes('.') &&
+            operatorContent !== '' &&
+            equalContent === '') {
+            displayNum2.textContent += eventKey;
+        } else if (num2Content === '' &&
+            !num2Content.includes('.') &&
+            operatorContent !== '') {
+            displayNum2.textContent += `0${eventKey}`;
+        }
+    } else if (eventKey === 'Escape') {
         resetValues();
+
+    } else if (eventKey === 'Backspace') {
+        if (num1Content.length > 1 &&
+            num2Content === '' &&
+            operatorContent === '') {
+            displayNum1.textContent =
+                displayNum1.textContent.slice(0, num1Content.length - 1);
+        } else if (num1Content.length === 1 &&
+            num2Content === '' &&
+            operatorContent === '') {
+            displayNum1.textContent = '0';
+        } else if (equalContent !== '') {
+            resetValues();
+        } else if (num2Content.length > 1 &&
+            operatorContent !== '') {
+            displayNum2.textContent =
+                displayNum2.textContent.slice(0, num2Content.length - 1);
+        } else if (num2Content.length === 1 &&
+            operatorContent !== '') {
+            displayNum2.textContent = '0';
+        }
+    } else if (eventKey === '=' || eventKey === 'Enter') {
+        if (operatorContent !== '' && num2Content !== '' && equalContent === '') {
+            displayEqual.textContent = '=';
+            equalBtn.style.background = equalBtnClkBg;
+            operateResult = operate(num1Content, num2Content, operatorContent);
+            displayResult.textContent = operateResult;
+        } else if (numResult !== '' &&
+            equalContent !== '') {
+            operateResult = operate(num1Content, num2Content, operatorContent);
+            displayNum1.textContent = operateResult;
+            displayResult.textContent = operate(operateResult, num2Content, operatorContent);
+        }
+
     }
+
+
+    if (eventKey === '+' || eventKey === '-' || eventKey === '/' || eventKey === '*') {
+        let operatorSymbol = '';
+
+        if (eventKey === '+' || eventKey === '-') {
+            operatorSymbol = eventKey;
+        } else if (eventKey === '/') {
+            operatorSymbol = '÷';
+        } else if (
+            eventKey === '*') {
+            operatorSymbol = '×';
+        }
+
+        //Press another operator after pressing equal sign
+        if (numResult !== '' &&
+            equalContent !== '') {
+            displayOperator.textContent = operatorSymbol;
+            equalBtn.style.background = equalBtnDefBg;
+            displayNum1.textContent = numResult;
+            displayEqual.textContent = '';
+            displayNum2.textContent = '';
+            displayResult.textContent = '';
+            displayResult.textContent = operate(num1Content, num2Content, operatorContent);
+        }
+        //Press another operator without pressing equal sign
+        else if (num1Content !== '' &&
+            num2Content !== '') {
+            operateResult = operate(num1Content, num2Content, operatorContent);
+            // Error handler when dividing by zero then pressing another key operator
+            if (operateResult === 'Math Error') {
+                resetValues();
+                displayResult.textContent = 'Math Error';
+            } else {
+                displayOperator.textContent = operatorSymbol;
+                displayResult.textContent = operateResult;
+                displayNum1.textContent = operateResult;
+                displayNum2.textContent = '';
+            }
+            //Press operator after num1
+        } else if (num1Content !== '' && num2Content === '') {
+            displayOperator.textContent = operatorSymbol;
+        }
+
+    }
+
+
 })
-
-
 
 
 
@@ -210,6 +279,17 @@ const resetValues = () => {
     equalBtn.style.background = equalBtnDefBg;
 }
 
+const add = (num1, num2) => (num1 + num2)
+const subtract = (num1, num2) => (num1 - num2)
+const multiply = (num1, num2) => (num1 * num2)
+const divide = (num1, num2) => {
+    if (num2 === 0) {
+        resetValues();
+        return 'Math Error';
+    } else {
+        return num1 / num2;
+    }
+}
 
 const operate = (num1, num2, operator) => {
     let parseNum1 = parseFloat(num1);
@@ -218,31 +298,24 @@ const operate = (num1, num2, operator) => {
 
     switch (operator) {
         case '+':
-            result = parseNum1 + parseNum2;
+            result = add(parseNum1, parseNum2);
             break;
         case '-':
-            result = parseNum1 - parseNum2;
+            result = subtract(parseNum1, parseNum2);
             break;
         case '×':
-            result = parseNum1 * parseNum2;
+            result = multiply(parseNum1, parseNum2);
             break;
         case '÷':
-            if (parseNum2 === 0) {
-                resetValues();
-                return 'Math Error';
-            } else {
-                result = parseNum1 / parseNum2;
-            }
+            result = divide(parseNum1, parseNum2);
             break;
     }
 
-    // Check if the result contains a decimal point and has more than 10 characters
     if (result.toString().includes('.') && result.toString().length > 10) {
         result = result.toFixed(4);
     }
 
     return result;
 }
-
 
 
